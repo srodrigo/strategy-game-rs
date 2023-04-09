12dc753df798e8683441cc609f4d04475b0aa8ed
Sergio Rodrigo Royo <sergio.rodrigo.royo@gmail.com>
2023-04-09 10:46:15 +0100

    feat: render 2x2 battlefield

--- 01-battlefield/strategy-game-rs/src/main.rs
+++ 01-battlefield/strategy-game-rs/src/main.rs
@@ -1,22 +1,79 @@
 use bevy::prelude::*;
 
+enum TileType {
+    Brown1,
+    Brown2,
+    Brown3,
+    Brown4,
+}
+
+struct Tile {
+    index: usize,
+}
+
+impl Tile {
+    fn from_type(tile_type: TileType) -> Tile {
+        match tile_type {
+            TileType::Brown1 => Tile { index: 0 },
+            TileType::Brown2 => Tile { index: 1 },
+            TileType::Brown3 => Tile { index: 2 },
+            TileType::Brown4 => Tile { index: 3 },
+        }
+    }
+}
+
 fn create_battlefield_system(
     mut commands: Commands,
     asset_server: Res<AssetServer>,
     mut texture_atlases: ResMut<Assets<TextureAtlas>>,
 ) {
+    commands.spawn(Camera2dBundle::default());
+
+    const NUM_COLUMNS: usize = 20;
+    const NUM_ROWS: usize = 20;
+    const TILE_SIZE: f32 = 16.0;
+
     let tiles_handle = asset_server.load("Tiles/FullTileset.png");
-    let tiles_atlas =
-        TextureAtlas::from_grid(tiles_handle, Vec2::new(16.0, 16.0), 20, 20, None, None);
+    let tiles_atlas = TextureAtlas::from_grid(
+        tiles_handle,
+        Vec2::new(TILE_SIZE, TILE_SIZE),
+        NUM_COLUMNS,
+        NUM_ROWS,
+        None,
+        None,
+    );
     let tiles_atlas_handle = texture_atlases.add(tiles_atlas);
 
-    commands.spawn(Camera2dBundle::default());
-    commands.spawn(SpriteSheetBundle {
-        texture_atlas: tiles_atlas_handle,
-        sprite: TextureAtlasSprite::new(3),
-        transform: Transform::from_scale(Vec3::splat(4.0)),
-        ..default()
-    });
+    const SCALE: f32 = 4.0;
+    let tile_map: [[Tile; 2]; 2] = [
+        [
+            Tile::from_type(TileType::Brown1),
+            Tile::from_type(TileType::Brown3),
+        ],
+        [
+            Tile::from_type(TileType::Brown2),
+            Tile::from_type(TileType::Brown4),
+        ],
+    ];
+
+    for (y, row) in tile_map.iter().enumerate() {
+        for (x, col) in row.iter().enumerate() {
+            commands.spawn(SpriteSheetBundle {
+                texture_atlas: tiles_atlas_handle.clone(),
+                sprite: TextureAtlasSprite::new(col.index),
+                transform: Transform {
+                    translation: Vec3 {
+                        x: x as f32 * TILE_SIZE * SCALE,
+                        y: y as f32 * TILE_SIZE * SCALE,
+                        z: 0.0,
+                    },
+                    scale: Vec3::splat(SCALE),
+                    ..default()
+                },
+                ..default()
+            });
+        }
+    }
 }
 
 fn main() {
